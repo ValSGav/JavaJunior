6e2e19aaf58f31b029289ed5463da25a054799ad
Val Gavrilov <valentin-s-gavriliv@yandex.ru>
2024-03-14 23:29:37 +0300

    format text

--- src/main/java/ru/gb/lesson1/homework/Homework.java
+++ src/main/java/ru/gb/lesson1/homework/Homework.java
@@ -31,60 +31,60 @@ public class Homework {
         System.out.println(findFirstPersons(persons));
         System.out.println(findTopDepartment(persons));
     }
-        /**
-         * Реалзиовать методы, описанные ниже:
-         */
+    /**
+     * Реалзиовать методы, описанные ниже:
+     */
 
-        /**
-         * Вывести на консоль отсортированные (по алфавиту) имена персонов
-         */
-        public static void printNamesOrdered(List<Streams.Person> persons) {
-            persons.stream()
-                    .sorted(Comparator.comparing(Streams.Person::getName))
-                    .forEach(System.out::println);
-        }
+    /**
+     * Вывести на консоль отсортированные (по алфавиту) имена персонов
+     */
+    public static void printNamesOrdered(List<Streams.Person> persons) {
+        persons.stream()
+                .sorted(Comparator.comparing(Streams.Person::getName))
+                .forEach(System.out::println);
+    }
 
-        /**
-         * В каждом департаменте найти самого взрослого сотрудника.
-         * Вывести на консоль мапипнг department -> personName
-         * Map<Department, Person>
-         */
-        public static Map<Streams.Department, Streams.Person> printDepartmentOldestPerson(List<Streams.Person> persons) {
-            return persons.stream()
-                    .collect(Collectors.groupingBy(Streams.Person::getDepartment
-                            , Collectors.maxBy(Comparator.comparing(Streams.Person::getAge))))
-                    .entrySet()
-                    .stream()
-                    .collect(Collectors.toMap(k-> k.getKey(), v-> v.getValue().get()));
-        }
+    /**
+     * В каждом департаменте найти самого взрослого сотрудника.
+     * Вывести на консоль мапипнг department -> personName
+     * Map<Department, Person>
+     */
+    public static Map<Streams.Department, Streams.Person> printDepartmentOldestPerson(List<Streams.Person> persons) {
+        return persons.stream()
+                .collect(Collectors.groupingBy(Streams.Person::getDepartment
+                        , Collectors.maxBy(Comparator.comparing(Streams.Person::getAge))))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(k -> k.getKey(), v -> v.getValue().get()));
+    }
 
-        /**
-         * Найти 10 первых сотрудников, младше 30 лет, у которых зарплата выше 50_000
-         */
-        public static List<Streams.Person> findFirstPersons(List<Streams.Person> persons) {
-            int limitOfPersonsCount = 10;
-            return persons.stream()
-                    .filter(it-> it.getSalary()>50_000)
-                    .filter(it->it.getAge()<30)
-                    .limit(limitOfPersonsCount)
-                    .collect(Collectors.toList());
-        }
+    /**
+     * Найти 10 первых сотрудников, младше 30 лет, у которых зарплата выше 50_000
+     */
+    public static List<Streams.Person> findFirstPersons(List<Streams.Person> persons) {
+        int limitOfPersonsCount = 10;
+        return persons.stream()
+                .filter(it -> it.getSalary() > 50_000)
+                .filter(it -> it.getAge() < 30)
+                .limit(limitOfPersonsCount)
+                .collect(Collectors.toList());
+    }
 
-        /**
-         * Найти депаратмент, чья суммарная зарплата всех сотрудников максимальна
-         */
-        public static Optional<Streams.Department> findTopDepartment(List<Streams.Person> persons) {
-           return Optional.ofNullable(persons.stream()
-                   .collect(Collectors.groupingBy(Streams.Person::getDepartment
-                           , Collectors.summingDouble(Streams.Person::getSalary)))
-                   .entrySet()
-                   .stream()
-                   .sorted(Comparator.comparing(Map.Entry<Streams.Department, Double>::getValue).reversed())
-                   .findFirst()
-                   .get()
-                   .getKey());
-
-        }
+    /**
+     * Найти депаратмент, чья суммарная зарплата всех сотрудников максимальна
+     */
+    public static Optional<Streams.Department> findTopDepartment(List<Streams.Person> persons) {
+        return Optional.ofNullable(persons.stream()
+                .collect(Collectors.groupingBy(Streams.Person::getDepartment
+                        , Collectors.summingDouble(Streams.Person::getSalary)))
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Map.Entry<Streams.Department, Double>::getValue).reversed())
+                .findFirst()
+                .get()
+                .getKey());
 
     }
+
+}
 
